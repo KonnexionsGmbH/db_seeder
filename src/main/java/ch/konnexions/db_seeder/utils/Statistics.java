13de4819769e71e48a0d49c3c20b4396172e7176
walter-weinmann <walter@konnexions.ch>
2020-07-09 21:12:02 +0200

    Statistics and complete run.

---
+++ src/main/java/ch/konnexions/db_seeder/utils/Statistics.java
@@ -0,0 +1,140 @@
+/*
+ *
+ */
+
+package ch.konnexions.db_seeder.utils;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.log4j.Logger;
+
+import ch.konnexions.db_seeder.AbstractDatabaseSeeder;
+
+/**
+ * This class is used to record the statisticss of the db_seeder runs.
+ */
+public class Statistics {
+  private static Logger               logger    = Logger.getLogger(Statistics.class);
+
+  private final Config                config;
+
+  private final String                dbmsTickerSymbol;
+  private final Map<String, String[]> dbmsValues;
+
+  private final DateTimeFormatter     formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.nnnnnnnnn");
+
+  private final LocalDateTime         startDateTime;
+  private CSVPrinter                  statisticsFile;
+
+  /**
+   * Constructs a Statistics object using the given {@link Config} object.
+   * @param config the {@link Config} object
+   * @param dbmsTickerSymbol the DBMS ticker symbol
+   * @param dbmsValues the DBMS related values DBMS name and client / embedded remark
+   */
+  public Statistics(Config config, String dbmsTickerSymbol, Map<String, String[]> dbmsValues) {
+    this.config           = config;
+    this.dbmsTickerSymbol = dbmsTickerSymbol;
+    this.dbmsValues       = dbmsValues;
+    this.startDateTime    = LocalDateTime.now();
+
+    createStatisticsFile();
+
+    openStatisticsFile();
+  }
+
+  public final void createMeasuringEntry() {
+    String methodName = new Object() {
+    }.getClass().getName();
+
+    logger.debug(String.format(AbstractDatabaseSeeder.FORMAT_METHOD_NAME, methodName) + "- Start");
+
+    try {
+      LocalDateTime endDateTime = LocalDateTime.now();
+
+      statisticsFile.printRecord(dbmsTickerSymbol,
+                                 dbmsValues.get(dbmsTickerSymbol)[0],
+                                 dbmsValues.get(dbmsTickerSymbol)[1],
+                                 Duration.between(startDateTime, endDateTime).toSeconds(),
+                                 startDateTime.format(formatter),
+                                 endDateTime.format(formatter));
+
+      statisticsFile.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    logger.debug(String.format(AbstractDatabaseSeeder.FORMAT_METHOD_NAME, methodName) + "- End");
+  }
+
+  /**
+   * Creates a new statistics file if none exists yet.
+   */
+  @SuppressWarnings("resource")
+  private final void createStatisticsFile() {
+    String methodName = new Object() {
+    }.getClass().getName();
+
+    logger.debug(String.format(AbstractDatabaseSeeder.FORMAT_METHOD_NAME, methodName) + "- Start");
+
+    String statisticsDelimiter = config.getFileStatisticsDelimiter();
+    String statisticsName      = config.getFileStatisticsName();
+
+    try {
+      Path    statisticsPath = Paths.get(statisticsName);
+
+      boolean isFileExisting = Files.exists(Paths.get(statisticsName));
+
+      if (!(isFileExisting)) {
+        Files.createFile(statisticsPath);
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(statisticsName, false));
+
+        new CSVPrinter(bufferedWriter,
+            CSVFormat.EXCEL.withDelimiter(statisticsDelimiter.charAt(0)).withHeader(config.getFileStatisticsHeader().split(statisticsDelimiter)));
+
+        bufferedWriter.close();
+
+        logger.info(String.format(AbstractDatabaseSeeder.FORMAT_METHOD_NAME, methodName) + "  missing statistics file created: file name="
+            + config.getFileStatisticsName());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    logger.debug(String.format(AbstractDatabaseSeeder.FORMAT_METHOD_NAME, methodName) + "- End");
+  }
+
+  private final void openStatisticsFile() {
+    String statisticsDelimiter = config.getFileStatisticsDelimiter();
+    String statisticsName      = config.getFileStatisticsName();
+
+    try {
+      boolean isFileExisting = Files.exists(Paths.get(statisticsName));
+
+      if (!(isFileExisting)) {
+        logger.error("fatal error: program abort =====> statistics file \"" + statisticsName + "\" is missing <=====");
+        System.exit(1);
+      }
+
+      BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(statisticsName, true));
+      statisticsFile = new CSVPrinter(bufferedWriter, CSVFormat.EXCEL.withDelimiter(statisticsDelimiter.charAt(0)));
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+}
