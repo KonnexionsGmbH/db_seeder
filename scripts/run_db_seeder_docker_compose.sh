10918c38c9738883f7eaaeffc85796f5619bb856
walter-weinmann <walter.weinmann@gmail.com>
2020-10-04 00:48:40 +0200

    Version 2.5.2

--- scripts/run_db_seeder_docker_compose.sh
+++ scripts/run_db_seeder_docker_compose.sh
@@ -24,19 +24,20 @@ export DB_SEEDER_CONNECTION_HOST=db_seeder_db
 
 export DB_SEEDER_FILE_JSON_NAME=resources/json/db_seeder_schema.company.json
 export DB_SEEDER_FILE_STATISTICS_NAME=/Transfer_db_seeder/db_seeder_local_client_${DB_SEEDER_RELEASE}_company_$(date +"%Y.%m.%d_%H.%M.%S").tsv
-./run_db_seeder.sh complete_client yes 2
+#./run_db_seeder.sh complete_client yes 2
+./run_db_seeder.sh agens yes 2
 
-export DB_SEEDER_FILE_JSON_NAME=resources/json/db_seeder_schema.syntax.json
-export DB_SEEDER_FILE_STATISTICS_NAME=/Transfer_db_seeder/db_seeder_local_client_${DB_SEEDER_RELEASE}_syntax_$(date +"%Y.%m.%d_%H.%M.%S").tsv
-./run_db_seeder.sh complete_client yes 2
+#export DB_SEEDER_FILE_JSON_NAME=resources/json/db_seeder_schema.syntax.json
+#export DB_SEEDER_FILE_STATISTICS_NAME=/Transfer_db_seeder/db_seeder_local_client_${DB_SEEDER_RELEASE}_syntax_$(date +"%Y.%m.%d_%H.%M.%S").tsv
+#./run_db_seeder.sh complete_client yes 2
 
 export DB_SEEDER_FILE_JSON_NAME=resources/json/db_seeder_schema.company.json
 export DB_SEEDER_FILE_STATISTICS_NAME=/Transfer_db_seeder/db_seeder_local_embedded_${DB_SEEDER_RELEASE}_company_$(date +"%Y.%m.%d_%H.%M.%S").tsv
 ./run_db_seeder.sh complete_emb yes 2
 
-export DB_SEEDER_FILE_JSON_NAME=resources/json/db_seeder_schema.company.json
-export DB_SEEDER_FILE_STATISTICS_NAME=/Transfer_db_seeder/db_seeder_local_presto_${DB_SEEDER_RELEASE}_company_$(date +"%Y.%m.%d_%H.%M.%S").tsv
-./run_db_seeder.sh complete_presto yes 2
+#export DB_SEEDER_FILE_JSON_NAME=resources/json/db_seeder_schema.company.json
+#export DB_SEEDER_FILE_STATISTICS_NAME=/Transfer_db_seeder/db_seeder_local_presto_${DB_SEEDER_RELEASE}_company_$(date +"%Y.%m.%d_%H.%M.%S").tsv
+#./run_db_seeder.sh complete_presto yes 2
 
 echo ""
 echo "--------------------------------------------------------------------------------"
