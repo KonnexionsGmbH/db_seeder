34fb484bbb2bef0fb0b4a2fb5f2f449100b8cd8f
walter-weinmann <walter.weinmann@gmail.com>
2020-10-19 11:11:41 +0200

    Version 2.6.0

--- scripts/run_db_seeder_generate_schema.sh
+++ scripts/run_db_seeder_generate_schema.sh
@@ -16,7 +16,7 @@ export DB_SEEDER_RELEASE=2.6.0
 export DB_SEEDER_JAVA_CLASSPATH=".:lib/*:JAVA_HOME/lib"
 
 if [ -z "${HOME_ECLIPSE}" ]; then
-    export HOME_ECLIPSE=/snap/bin
+    export HOME_ECLIPSE=/opt/eclipse
 fi
 
 echo "================================================================================"
