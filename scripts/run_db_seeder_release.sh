8ba5d6ff2c88248e51eb63b5f5d306f7208828e6
Walter Weinmann <walter.weinmann@gmail.com>
2021-09-06 16:43:46 +0200

    Version 3.0.2

--- scripts/run_db_seeder_release.sh
+++ scripts/run_db_seeder_release.sh
@@ -23,7 +23,7 @@ echo "Generator."
 echo "--------------------------------------------------------------------------------"
 export DB_SEEDER_FILE_JSON_NAME=resources/json/db_seeder_schema.company_5400.json
 
-#./scripts/run_db_seeder_generate_schema.sh
+./scripts/run_db_seeder_generate_schema.sh
 
 echo "--------------------------------------------------------------------------------"
 echo "Constraints included."
@@ -31,54 +31,54 @@ echo "--------------------------------------------------------------------------
 export DB_SEEDER_DROP_CONSTRAINTS=no
 export DB_SEEDER_FILE_STATISTICS_NAME=resources/statistics/db_seeder_cmd_complete_company_9.9.9_wsl2.tsv
 
-#rm -rf resources/statistics/db_seeder_cmd_complete_company_9.9.9_wsl2.tsv
+rm -rf resources/statistics/db_seeder_cmd_complete_company_9.9.9_wsl2.tsv
 
-#./run_db_seeder.sh agens            yes 1
-#./run_db_seeder.sh cockroach        yes 1
-#./run_db_seeder.sh cratedb          yes 1
-#./run_db_seeder.sh cubrid           yes 1
-#./run_db_seeder.sh derby            yes 1
-#./run_db_seeder.sh derby_emb        yes 1
-#./run_db_seeder.sh exasol           yes 1
-#./run_db_seeder.sh firebird         yes 1
-#./run_db_seeder.sh h2               yes 1
-#./run_db_seeder.sh h2_emb           yes 1
-#./run_db_seeder.sh hsqldb           yes 1
-#./run_db_seeder.sh hsqldb_emb       yes 1
-#./run_db_seeder.sh ibmdb2           yes 1
-#./run_db_seeder.sh informix         yes 1
-#./run_db_seeder.sh mariadb          yes 1
-#./run_db_seeder.sh mimer            yes 1
-#./run_db_seeder.sh monetdb          yes 1
-#./run_db_seeder.sh mysql            yes 1
-#./run_db_seeder.sh mysql_trino      yes 1
-#./run_db_seeder.sh omnisci          yes 1
-#./run_db_seeder.sh oracle           yes 1
-#./run_db_seeder.sh oracle_trino     yes 1
-#./run_db_seeder.sh percona          yes 1
-#./run_db_seeder.sh postgresql       yes 1
-#./run_db_seeder.sh postgresql_trino yes 1
-#./run_db_seeder.sh sqlite           yes 1
-#./run_db_seeder.sh sqlserver        yes 1
-#./run_db_seeder.sh sqlserver_trino  yes 1
-#./run_db_seeder.sh timescale        yes 1
-## Java 15:
-#./run_db_seeder.sh voltdb           yes 0
-#./run_db_seeder.sh yugabyte         yes 1
+./run_db_seeder.sh agens            yes 1
+./run_db_seeder.sh cockroach        yes 1
+./run_db_seeder.sh cratedb          yes 1
+./run_db_seeder.sh cubrid           yes 1
+./run_db_seeder.sh derby            yes 1
+./run_db_seeder.sh derby_emb        yes 1
+./run_db_seeder.sh exasol           yes 1
+./run_db_seeder.sh firebird         yes 1
+./run_db_seeder.sh h2               yes 1
+./run_db_seeder.sh h2_emb           yes 1
+./run_db_seeder.sh hsqldb           yes 1
+./run_db_seeder.sh hsqldb_emb       yes 1
+./run_db_seeder.sh ibmdb2           yes 1
+./run_db_seeder.sh informix         yes 1
+./run_db_seeder.sh mariadb          yes 1
+./run_db_seeder.sh mimer            yes 1
+./run_db_seeder.sh monetdb          yes 1
+./run_db_seeder.sh mysql            yes 1
+./run_db_seeder.sh mysql_trino      yes 1
+./run_db_seeder.sh omnisci          yes 1
+./run_db_seeder.sh oracle           yes 1
+./run_db_seeder.sh oracle_trino     yes 1
+./run_db_seeder.sh percona          yes 1
+./run_db_seeder.sh postgresql       yes 1
+./run_db_seeder.sh postgresql_trino yes 1
+./run_db_seeder.sh sqlite           yes 1
+./run_db_seeder.sh sqlserver        yes 1
+./run_db_seeder.sh sqlserver_trino  yes 1
+./run_db_seeder.sh timescale        yes 1
+# Java 15:
+./run_db_seeder.sh voltdb           yes 0
+./run_db_seeder.sh yugabyte         yes 1
 
 echo "--------------------------------------------------------------------------------"
 echo "Constraints excluded."
 echo "--------------------------------------------------------------------------------"
 export DB_SEEDER_DROP_CONSTRAINTS=yes
 
-#./run_db_seeder.sh agens            yes 1
-#./run_db_seeder.sh cubrid           yes 1
-#./run_db_seeder.sh derby            yes 1
-#./run_db_seeder.sh derby_emb        yes 1
-#./run_db_seeder.sh exasol           yes 1
-#./run_db_seeder.sh firebird         yes 1
-#./run_db_seeder.sh hsqldb           yes 1
-#./run_db_seeder.sh hsqldb_emb       yes 1
+./run_db_seeder.sh agens            yes 1
+./run_db_seeder.sh cubrid           yes 1
+./run_db_seeder.sh derby            yes 1
+./run_db_seeder.sh derby_emb        yes 1
+./run_db_seeder.sh exasol           yes 1
+./run_db_seeder.sh firebird         yes 1
+./run_db_seeder.sh hsqldb           yes 1
+./run_db_seeder.sh hsqldb_emb       yes 1
 ./run_db_seeder.sh ibmdb2           yes 1
 ./run_db_seeder.sh informix         yes 1
 ./run_db_seeder.sh mariadb          yes 1
