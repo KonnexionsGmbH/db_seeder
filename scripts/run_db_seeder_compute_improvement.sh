82dcd01024592893f888a6676646abee3630279b
Walter Weinmann <walter.weinmann@gmail.com>
2022-02-28 08:08:11 +0100

    Version 3.0.6

--- scripts/run_db_seeder_compute_improvement.sh
+++ scripts/run_db_seeder_compute_improvement.sh
@@ -4,8 +4,8 @@ set -e
 
 # ------------------------------------------------------------------------------
 #
-# run_db_seeder_compute_improvement.sh: Compute runtime improvement with and 
-#                                       without constraints. 
+# run_db_seeder_compute_improvement.sh: Compute runtime improvement with and
+#                                       without constraints.
 #
 # ------------------------------------------------------------------------------
 
@@ -17,19 +17,15 @@ fi
 export DB_SEEDER_FILE_IMPROVEMENT_NAME_DEFAULT=resources/statistics/db_seeder_bash_improvement_company_9.9.9_vmware_wsl2.tsv
 if [ -z "${DB_SEEDER_FILE_IMPROVEMENT_NAME}" ]; then
     export DB_SEEDER_FILE_IMPROVEMENT_NAME=${DB_SEEDER_FILE_IMPROVEMENT_NAME_DEFAULT}
-fi 
+fi
 
 export DB_SEEDER_FILE_STATISTICS_NAME_DEFAULT=resources/statistics/db_seeder_bash_complete_company_9.9.9_vmware_wsl2.tsv
 export DB_SEEDER_FILE_STATISTICS_NAME="$1"
 if [ -z "${DB_SEEDER_FILE_STATISTICS_NAME}" ]; then
     export DB_SEEDER_FILE_STATISTICS_NAME=${DB_SEEDER_FILE_STATISTICS_NAME_DEFAULT}
-fi 
+fi
 
 export DB_SEEDER_JAVA_CLASSPATH=".:lib/*:JAVA_HOME/lib"
-
-if [ -z "${HOME_ECLIPSE}" ]; then
-    export HOME_ECLIPSE=/opt/eclipse
-fi
 
 echo "================================================================================"
 echo "Start $0"
@@ -51,7 +47,7 @@ rm -f db_seeder.log
 
 if ! (java -cp "{${DB_SEEDER_JAVA_CLASSPATH}}" ch.konnexions.db_seeder.ComputeImprovement); then
     exit 255
-fi    
+fi
 
 echo "--------------------------------------------------------------------------------"
 date +"DATE TIME : %d.%m.%Y %H:%M:%S"
